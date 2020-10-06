f8310df4e218cf00a690ada7595336f0c84ecf31
prashant577 <prashantkumarseemala@gmail.com>
2020-10-06 13:29:57 +0530

    homework of class 2,3 and 4

---
+++ Class3_Homework/src/Sum_problem.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Sum_problem {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int sum;
+		int again;
+		
+		do {
+			sum=a+b;
+			System.out.println(sum);
+			System.out.println("do you further operation");
+			again=sc.nextInt();
+			
+		}while(again==1 || again==0);
+		
+			
+	}
+
+
+}
